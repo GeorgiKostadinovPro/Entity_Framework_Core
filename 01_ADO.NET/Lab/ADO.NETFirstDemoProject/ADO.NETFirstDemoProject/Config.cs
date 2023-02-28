3f3770f5639bda0846e4a5ead81f08e53fc21cba
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2023-02-28 20:59:00 +0200

    ADO.NET basic project demo

---
+++ 01_ADO.NET/Lab/ADO.NETFirstDemoProject/ADO.NETFirstDemoProject/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace ADO.NETFirstDemoProject
+{
+    public static class Config
+    {
+        public static string ConnectionString = @"Server=.;Database=Softuni;Integrated Security=True;";
+    }
+}
