f48591ba39eec53e4bb431aec37ceead005dbd9f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2023-03-03 22:04:53 +0200

    created custom ORM based on EF Core 6

---
+++ 02_ORM_Fundamentals/Exercises/MiniORM.App/Data/Models/EmployeeProject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniORM.App.Data.Models;
+
+public class EmployeeProject
+{
+    [Key]
+    [ForeignKey(nameof(Employee))]
+    public int EmployeeId { get; set; }
+
+    public virtual Employee Employee { get; set; }
+
+    [Key]
+    [ForeignKey(nameof(Project))]
+    public int ProjectId { get; set; }
+
+    public virtual Employee Project { get; set; }
+}
---
+++ 02_ORM_Fundamentals/Exercises/MiniORM.App/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Models;
+
+namespace MiniORM.App;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        SoftUniDbContext dbContext = new SoftUniDbContext(Config.ConnectionString);
+
+        Employee newEmployee = dbContext.Employees
+            .FirstOrDefault(e => e.FirstName == "Test"
+                              && e.LastName == "Test");
+
+        if (newEmployee == null)
+        {
+            Console.WriteLine("Employee was not found!");
+            return;
+        }
+
+        //dbContext.Employees.Add(newEmployee);
+
+        dbContext.Employees.Remove(newEmployee);
+
+        dbContext.SaveChanges();
+    }
+}
