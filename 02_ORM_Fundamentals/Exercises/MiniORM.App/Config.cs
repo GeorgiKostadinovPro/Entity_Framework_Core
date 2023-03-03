1b573f690a39bc7b7fe6dc624c05d5f65ed114ee
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2023-03-03 22:05:54 +0200

    created custom ORM based on EF Core 6

---
+++ 02_ORM_Fundamentals/Exercises/MiniORM.App/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App
+{
+    public static class Config
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-8A3LHOC\SQLEXPRESS;Database=MiniORM;Integrated Security=True;Encrypt=False;";
+    }
+}
---
+++ 02_ORM_Fundamentals/Exercises/MiniORM/ExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM;
+
+internal static class ExceptionMessages
+{
+    public static string EntityNullException = "Entity cannot be null!";
+
+    public static string InvalidEntitiesException = "{0} Invalid entities found in {1}!";
+}
